71bbdd749e08045c1f7e19dfa3edc7f3d66aa939
Johan Sundell <johan@pixpro.net>
2019-03-04 13:05:43 +0100

    Moved some stuff

--- main.go
+++ main.go
@@ -3,12 +3,27 @@ package main
 import (
 	"fmt"
 	"time"
-
-	"github.com/johansundell/go-play-javascript/pkg/mypi"
 )
+
+func term(n int) float64 {
+	i := float64(n)*2 + 1
+	if n%2 == 0 {
+		return -4 / ((i - 1) * i * (i + 1))
+	} else {
+		return 4 / ((i - 1) * i * (i + 1))
+	}
+}
+
+func GetPi(n int) float64 {
+	f := 3.0
+	for i := 1; i <= n; i++ {
+		f += term(i)
+	}
+	return f
+}
 
 func main() {
 	started := time.Now()
-	fmt.Println(mypi.GetPi(1000 * 1000 * 1000))
+	fmt.Println(GetPi(1000 * 1000 * 1000))
 	fmt.Println("total time taken is:", time.Since(started))
 }
